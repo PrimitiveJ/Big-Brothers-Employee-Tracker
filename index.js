3760dda187b6e90b75a08eb012a9033d90b7e048
Primitive J <hokecj@gmail.com>
2022-05-08 11:44:28 -0400

    created functions that interact with database

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const dotenv = require('dotenv') // npm i dotenv --save
         promptUser();
     });
 
-    //Inquirer Options to call functions with user input-------------------------------------------------------------------------------------------------------------------------------
+//Inquirer Options to call functions with user input-------------------------------------------------------------------------------------------------------------------------------
     function promptUser() {
         inquirer.prompt([
             {
@@ -67,3 +67,166 @@ const dotenv = require('dotenv') // npm i dotenv --save
             });
     }
 
+//Functions that interact with database-------------------------------------------------------------------------------------------------------------
+
+    // Displays a table of all departments with names and IDs (department specific IDs)
+    function viewallDepartments(){
+        const sql = `SELECT * FROM departments`;
+        db.query(sql,(err,result)=>{
+                if (err){
+                    throw err
+                }
+                console.table(result);
+                promptUser();
+            })
+        }
+    
+        // Displays all roles with job title, role id (job specific), department id (department employee belongs to) and salary
+        function viewallRoles(){
+            const sql=`SELECT r.title,r.salary,d.name,d.id FROM role AS r JOIN department AS d ON d.id = r.department_id`;
+        db.query(sql,(err,result)=>{
+            if (err)
+                throw err
+            
+           console.table(result);
+           question();
+        })
+        }
+    
+        // Displays a table showing employees
+        function viewallEmployees(){
+            const sql = `SELECT * FROM employees`;
+            db.query(sql,(err,result) => {
+                if(err){
+                    throw err
+                }
+                console.table(result);
+                promptUser();
+            })
+        }
+    
+        //Prompts user to enter department name and adds department to database
+        function addDepartments(){
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'deptName',
+                    message: 'What is the name of the department you wish to add?'
+                }
+            ]) 
+            .then((response) => {
+               
+                const sql=`INSERT INTO department (name) VALUES (?)`; //References name from department in schema.sql
+                db.query(sql,response.deptName,(err,result)=>{
+                    if (err)
+                    throw err
+                    console.table(result);
+                    question();
+              });  
+        
+            });  
+           
+        }
+    
+        //Prompts user to enter name, salary, and department for the role and adds role to the database
+        function addRole(){
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'title',
+                    message: 'What is the title of the role you wish to add?'
+                },
+                {
+                    type: 'input',
+                    name:'salary',
+                    message: 'what is the salary of the role ?'
+                },
+                {
+                    type: 'input',
+                    name: 'department_id',
+                    message: 'Which department does the role belong to ?'
+                    //Choices:     //department name(viewalldepartments)
+                }
+            ]) 
+            .then((response) => {
+               
+                const sql=`INSERT INTO role SET ? `;//use name from role in schema.sql/seeds(dept id ,title ,salary)
+                db.query(sql,response,(err,result)=>{
+                    if (err)
+                    throw err
+                    console.log(result);
+                    question();
+              }); 
+        
+            });  
+        }
+    
+        // Prompts user to enter employee's first name, last name, role and manager, then adds employee to the database
+        function addEmployee(){
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'first_name',
+                    message: 'Enter the first name of this employee:'
+                },
+                {
+                    type: 'input',
+                    name: 'last_name',
+                    message: 'Enter the last name of this employee:'
+                },
+                {
+                    type: 'input',
+                    name: 'role_id',
+                    message: 'What is the role of this employee?'
+                  //Choices:         //role title display-
+                },
+                {
+                    type: 'input',
+                    name: 'manager_id' ,
+                    message: 'Who is the manager of this employee?'
+                   //Choices:            //none,display employee first and last name
+                }
+            ])
+            .then((response) => {
+               
+                const sql=`INSERT INTO employee SET ?`;//use name from role in schema.sql/seeds(first name, lastname,role id, manager id)
+                db.query(sql,response,(err,result)=>{
+                    if (err)
+                    throw err
+                    console.log(result);
+                    question();
+              });  
+            });  
+        }
+    
+        // Prompts user to select an employee to update role and add that information to the database
+        function updateRole(){
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'employee',
+                    message: 'Which employee role do you wish to update?'
+                   //Choice:                   //need emp name 
+                },
+                {
+                    type: 'input',
+                    name:  "role"   ,
+                    message: 'Which role do you wish to assign the selected employee ?'
+                   //Choices:                    //need role title 
+                }
+         
+            ])
+        
+            .then((response) => {
+               
+                const sql=`UPDATE employee SET role_id = ? WHERE id = ?`;
+                db.query(sql,[response],(err,result)=>{
+                    if (err)
+                    throw err
+                    console.log(result);
+                    question(); 
+              });  
+            }); 
+           
+        }
+    
